4d1e1b1bbbb73a920b214293e69ed10990775704
Stella Rouzi <differentreality@gmail.com>
2018-10-30 15:43:55 +0200

    Add start/end date to events

--- db/migrate/20180822080035_create_events.rb
+++ db/migrate/20180822080035_create_events.rb
@@ -4,6 +4,8 @@ class CreateEvents < ActiveRecord::Migration[5.0]
       t.string :name
       t.string :object_id
       t.integer :shares
+      t.datetime :start_time
+      t.datetime :end_time
       t.datetime :posted_at
       t.references :page
 
